e38496da1fbef488532d234b51cc5ec1aa04b0d1
ali noori <ali.nouri213@gmail.com>
2021-09-19 16:42:52 +0430

    some change in templates

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,11 @@
-from .views import *
+from .views import all_products, product_detail, category_list
 from django.urls import path
 app_name = 'store'
 
 urlpatterns = [
+    path('', all_products, name='all_products'),
+    path('item/<slug:slug>/', product_detail, name='product_detail'),
+    path('search/<slug:category_slug>/', category_list, name='category_list'),
+
 
 ]
